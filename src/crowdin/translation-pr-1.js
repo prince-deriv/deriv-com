2fc228521ffbba37b0fb9fb5f5d1060228eb2535
Prince <prince@deriv.com>
2022-05-10 09:09:37 +0400

    chore: added new texts to be translated

---
+++ src/crowdin/translation-pr-1.js
@@ -0,0 +1,20 @@
+/* eslint-disable  */
+new_texts = [
+    <Localize translate_text="Your account is scheduled to be closed" />,
+    <Localize translate_text="OK, I understand" />,
+    <Localize translate_text="As part of the changes in our market offerings, we will be closing our UK clients’ accounts." />,
+    <Localize translate_text="What this means for you" />,
+    <Localize translate_text="You can no longer trade on Deriv or deposit funds into your account." />,
+    <Localize translate_text="If you had any open positions, we have closed them and refunded you." />,
+    <Localize translate_text="What you need to do now" />,
+    <Localize translate_text="Please proceed to withdraw all your funds from your account." />,
+    <Localize translate_text="You will lose access to your account when it gets closed, so be sure to withdraw all your funds." />,
+    <Localize translate_text="We do not offer our services in Belarus, Canada, Hong Kong, Israel, Jersey, Malaysia, Malta, Paraguay, Rwanda, the UAE, the UK, the USA or any country that has been identified by the Financial Action Task Force (FATF) as having strategic deficiencies." />,
+    <Localize translate_text="new text: localize" />,
+    <Localize translate_text="new_text: hi" />,
+    <Localize translate_text="new_text: hello" />,
+    <Localize translate_text="new_text: world" />,
+    <Localize translate_text="RTS28 2021" />,
+    <Localize translate_text="Deriv Investments (Europe) Limited has prepared the Financial disclosures report in accordance with the Capital Requirements Directive IV and the Capital Requirements Regulation. Read our report to understand how we comply with market discipline as a market participant." />,
+    <Localize translate_text="Financial Disclosures Annual Report 2021" />,
+]
